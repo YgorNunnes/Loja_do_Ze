a6faa5ab540b16fc4d2161bdbe171ba3b9d87e42
YgorNunnes <68921214+YgorNunnes@users.noreply.github.com>
2023-10-18 19:10:35 -0400

    Add files via upload

--- compras.c
+++ compras.c
@@ -1,36 +1,72 @@
 #include <stdio.h>
 #include "menu.h"
 
-void zerar_estoque(int arr[]) {
+void prod(int opc){
+    char* produtos[10] = {"Radiador","Velas de Ignição","Filtro de Óleo", "Pastilhas de Freio", "Filtro de Ar", "Bateria", "Alternador", "Pneus", "Calotas", "Faróis"};
+    printf("%s", produtos[opc]);
+}
+
+void zerar_estoque(int vet[]) {
     for (int i = 0; i < 10; i++) {
-        arr[i] = 0;
+        vet[i] = 0;
     }
 }
 
-void max_estoque(int arr[]) {
+
+void max_estoque(int vet[]) {
     for (int i = 0; i < 10; i++) {
-        arr[i] = 100;
+        vet[i] = 100;
     }
 }
 
-void finalizar(float total){
-    if(total >= 300){
-        printf("Total: %.2f. Desconto de %.2f \n", total, total * 0.05);
-        total = total - (total * 0.05);
-        pagar(total);
-    }
-    else{
-        printf("Total: %.2f\n", total);
-        pagar(total);
+
+void sub_estoque(int flag[], int estoque[]){
+    for(int i = 0; i < 10; i++){
+        if(flag[i] > 0){
+            estoque[i] -= flag[i];
+        }
     }
 }
 
-int preencher_estoque(){
+
+int consultar_estoque(){
+    int cod;
+    do{
+        printf(" -Menu de Consulta de Estoque- \n");
+        printf("Digite o código do produto: \n");
+        printf("Cód 0. Radiador\n");
+        printf("Cód 1. Velas de Ignição\n");
+        printf("Cód 2. Filtro de Óleo\n");
+        printf("Cód 3. Pastilhas de Freio\n");
+        printf("Cód 4. Filtro de Ar\n");
+        printf("Cód 5. Bateria\n");
+        printf("Cód 6. Alternador\n");
+        printf("Cód 7. Pneus\n");
+        printf("Cód 8. Calotas\n");
+        printf("Cód 9. Faróis\n");
+        printf("Cód 10.Finalizar\n");
+        printf(" \n");
+        scanf("%d", &cod);
+        
+        if(cod != 10){
+            printf("Produto selecionado: ");
+            prod(cod);
+            printf(" \n");
+            printf("Estoque disponível: %d\n", estoque[cod]);
+        }
+    }
+    while(cod != 10);
+    printf("Voltando ao menu...\n");
+    return main();
+}
+
+void preencher_estoque(){
     int cod = 0, qtd_repor = 0;
 
     while(cod != 11){
+        printf(" \n");
         printf("digite o código do produto que deseja reestocar:\n");
-        printf("Cód 0. %s\n", produtos[0]);
+        printf("Cód 0. Radiador\n");
         printf("Cód 1. Velas de Ignição\n");
         printf("Cód 2. Filtro de Óleo\n");
         printf("Cód 3. Pastilhas de Freio\n");
@@ -49,7 +85,9 @@ int preencher_estoque(){
             printf("Digite a quantidade desejada:\n");
             scanf("%d", &qtd_repor);
             estoque[cod] += qtd_repor;
-            printf("%d unidades adicionadas de %s.\n", estoque[cod], produtos[cod]);
+            printf("%d unidades adicionadas de ", estoque[cod]);
+            prod(cod);
+            printf(" \n");
         }
 
         else if(cod == 10){
@@ -57,7 +95,8 @@ int preencher_estoque(){
         }
         else if(cod == 11){
             printf("Finalizando... \n");
-            return main();
+            printf(" \n");
+            break;
         }
         else{
             printf("Código inválido! \n");
@@ -66,197 +105,225 @@ int preencher_estoque(){
 }
 
 
-
 int comprar() {
-    int escolha = -1, carrinho, qtd = 0;
+    int escolha = -1, carrinho = 0, qtd = 0, flag[10] = {0};
     float total = 0;
     
+    printf(" \n");
     printf("Bem-vindo ao Menu de Vendas do Ferro Velho do Zé!\n");
     printf("Desconto de 5%% para compras acima de R$300\n");
     printf("-------------------------------------------\n");
     
-    while (escolha != 11) {
+    while (escolha < 10) {
         printf("Digite o código do produto:\n");
-        printf("Cód 0. Radiador - R$200\n");
-        printf("Cód 1. Velas de Ignição - R$20\n");
-        printf("Cód 2. Filtro de Óleo - R$10\n");
+        printf("Cód 0. Radiador           - R$200\n");
+        printf("Cód 1. Velas de Ignição   - R$20,59\n");
+        printf("Cód 2. Filtro de Óleo     - R$10,50\n");
         printf("Cód 3. Pastilhas de Freio - R$50\n");
-        printf("Cód 4. Filtro de Ar - R$15\n");
-        printf("Cód 5. Bateria - R$80\n");
-        printf("Cód 6. Alternador - R$150\n");
-        printf("Cód 7. Pneu - R$300 \n");
-        printf("Cód 8. Calotas - R$50 cada\n");
-        printf("Cód 9. Faróis - R$290\n");
+        printf("Cód 4. Filtro de Ar       - R$15,99\n");
+        printf("Cód 5. Bateria            - R$80\n");
+        printf("Cód 6. Alternador         - R$150\n");
+        printf("Cód 7. Pneu               - R$300 \n");
+        printf("Cód 8. Calotas            - R$50\n");
+        printf("Cód 9. Faróis             - R$299,90\n");
         printf("10. Finalizar\n");
         printf("11. Cancelar\n");
         printf(" \n");
         scanf("%d", &escolha);
 
-        //printf("%d mamoes", estoque[escolha]);
-
         switch (escolha) {
             case 0:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Radiador (unidades disponíveis: %d)\n", estoque[escolha] - carrinho);            
                 printf("Digite a quantidade: \n");
                 scanf("%d", &qtd);
-                if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                if(estoque[escolha] <= 0 || estoque[escolha] < qtd){
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
+                }else{
+                    flag[0] += qtd;
                 }
-                carrinho = carrinho + qtd;
                 total = total + 200 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
-                break;   
+                printf(" \n");
+                break; 
+
             case 1:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Velas de Ignição (unidades disponíveis: %d)\n", estoque[escolha] - carrinho);            
                 printf("Digite a quantidade: \n");
                 scanf("%d", &qtd);
-                if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                if(estoque[escolha] <= 0 || estoque[escolha] < qtd){
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
+                }else{
+                    flag[1] += qtd;
                 }
-                carrinho = carrinho + qtd;
-                total = total + 20 * qtd;
+                total = total + 20.59 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;
+
             case 2:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Filtro de Óleo (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
                 scanf("%d", &qtd);
-                if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                if(estoque[escolha] <= 0 || estoque[escolha] < qtd){
+                    printf("Estoque Insuficiente\n");
                     break;
-                }
-                carrinho = carrinho + qtd;
-                total = total + 10 * qtd;
+                }else{
+                    flag[2] += qtd;
+                }                
+                total = total + 10.50 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;
+
             case 3:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Pastilhas de Freio (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
                 scanf("%d", &qtd);
-                if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                if(estoque[escolha] <= 0 || estoque[escolha] < qtd){
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
+                }else{
+                    flag[3] += qtd;
                 }
-                carrinho = carrinho + qtd;
                 total = total + 50 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;  
+
             case 4:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Filtro de Ar (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
-                scanf("%d", &qtd);
-                if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                scanf("%d", &qtd);   
+                if(estoque[escolha] <= 0 || estoque[escolha] < qtd){
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
+                }else{
+                    flag[4] += qtd;
                 }
-                carrinho = carrinho + qtd;
-                total = total + 15 * qtd;
+                total = total + 15.99 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;
+
             case 5:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Bateria (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
                 scanf("%d", &qtd);
+                
                 if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
                 }
-                carrinho = carrinho + qtd;
+                else{
+                    flag[5] += qtd;
+                }
                 total = total + 80 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;  
+
             case 6:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Alternador (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
-                scanf("%d", &qtd);
+                scanf("%d", &qtd);                
                 if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
                 }
-                carrinho = carrinho + qtd;
+                else{
+                    flag[6] += qtd;
+                }
                 total = total + 150 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;  
+
             case 7:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Pneus (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
-                scanf("%d", &qtd);
+                scanf("%d", &qtd);                
                 if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
                 }
-                carrinho = carrinho + qtd;
+                else{
+                    flag[7] += qtd;
+                }
                 total = total + 300 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;
+
             case 8:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Calotas (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
                 scanf("%d", &qtd);
                 if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
                 }
-                carrinho = carrinho + qtd;
+                else{
+                    flag[8] += qtd;
+                }
                 total = total + 50 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;
+
             case 9:
-                printf("Você selecionou: %s (unidades disponíveis: %d)\n", produtos[escolha], estoque[escolha]);            
+                printf("Você selecionou: Faróis (unidades disponíveis: %d)\n", estoque[escolha]);            
                 printf("Digite a quantidade: \n");
                 scanf("%d", &qtd);
                 if(estoque[escolha] <= 0 || estoque[escolha] <= carrinho){
-                    //printf("###%d ###%d", estoque[escolha], carrinho);
-                    printf("Estoque Insuficiente");
+                    printf("Estoque Insuficiente\n");
+                    printf(" \n");
                     break;
                 }
-                carrinho = carrinho + qtd;
-                total = total + 290 * qtd;
+                else{
+                    flag[9] += qtd;
+                }
+                total = total + 299.90 * qtd;
                 printf("Carrinho: R$%.2f\n", total);
-                estoque[escolha] = estoque[escolha] - carrinho; 
-                //printf("## quantia restante: %d\n", estoque[escolha]);
+                printf(" \n");
                 break;
+
             case 10:
-                finalizar(total);
+                if(total >= 300){
+                    sub_estoque(flag, estoque);
+                    printf("Total: R$%.2f. Desconto de R$%.2f \n", total - (total * 0.05), total * 0.05);
+                    printf(" \n");
+                    total = total - (total * 0.05);
+                    pagar(total);
+                }
+                else{
+                    sub_estoque(flag, estoque);
+                    printf("Total: R$%.2f\n", total);
+                    printf(" \n");
+                    pagar(total);
+                }
                 break;  
 
             case 11:
                 printf("Voltando para o menu inicial...\n");
+                printf(" \n");
                 break;
+                
             default:
-                printf("Escolha Inválida!");
+                printf("Escolha Inválida!\n");
+                printf(" \n");
                 break;
         }
     printf("\n");
     }
-    return main();
+    return 0;
 }
--- pagamento.c
+++ pagamento.c
@@ -1,9 +1,95 @@
 #include <stdio.h>
 #include "menu.h"
+#include <stdlib.h>
+#include <time.h>
 
+void sorteio(float cliente[]){
+    int cliente_sorteado = 0, flag = 0;
+    srand(time(0));
+
+    cliente_sorteado = rand()%9;
+
+    if(cliente[cliente_sorteado] == 0){
+        for(int i = 0; i < 10; i++){
+            if(cliente[i] != 0){
+                printf("Cliente numero %d foi o ganhador. A divida de R$%.2f foi abatida.\n", i, cliente[i]);
+                printf(" \n");
+                flag++;
+                cliente[i] -= cliente[i];
+                break;
+            }
+        }
+        if(flag == 0){
+            printf("Nao ha clientes endividados para sortear.\n");
+        }
+    }
+    else{
+        printf("Cliente numero %d foi o ganhador. A divida de R$%.2f foi abatida.\n", cliente_sorteado, cliente[cliente_sorteado]);
+        printf(" \n");
+        cliente[cliente_sorteado] -= cliente[cliente_sorteado];
+    }
+
+}
+
+
+int pagar_debitos(){
+    int cod_cliente;
+    float valor_a_pagar;
+    do{
+        printf("Digite seu codigo de cliente: \n");
+        scanf("%d", &cod_cliente);
+        printf("Seu saldo devedor e de? R$%.2f\n", vet_crediario[cod_cliente]);
+
+        if(vet_crediario[cod_cliente] == 0){
+            printf("Nao e necessario efetuar nenhum pagamento. (Nenhuma divida encontrada)\n");
+        }
+        else{
+            while(1){
+                printf("Quanto voce deseja pagar?\n");
+                scanf("%f", &valor_a_pagar);
+
+                if(valor_a_pagar > vet_crediario[cod_cliente]){
+                    printf("Nao e aceitamos pagamentos acima do valor devido. \n");
+                    printf("saldo devedor atual: R$%.2f\n", vet_crediario[cod_cliente]);
+                }
+                else{
+                    vet_crediario[cod_cliente] -= valor_a_pagar;
+                    printf("Pagamento efetuado! \n");
+                    printf("saldo devedor atual: R$%.2f\n", vet_crediario[cod_cliente]);
+                    return 0;
+                }
+            }
+        }
+        
+    }while(cod_cliente != 10);
+    return 0;
+}
+
+
+int consulta_debitos(){
+    int cod_cliente;
+    while(1){
+        printf("Digite seu código de cliente: (10 - Sair)\n");
+        scanf("%d", &cod_cliente);
+
+        if(cod_cliente >= 0 && cod_cliente < 10){
+            printf(" \n");
+            printf("Seu saldo negativo é de: R$%.2f\n", vet_crediario[cod_cliente]);
+        }
+        else if(cod_cliente == 10){
+            printf(" \n");
+            printf("Fechando consulta...\n");
+            printf(" \n");
+            return 0;
+        }
+        else{
+            printf("Código inválido\n");
+        }
+    }
+}
 
 int pagar(float valor){
-    int forma_pagamento, cod_cliente, perfil;
+    int forma_pagamento, cod_cliente;
 
     while(1){
         printf("Escolha a forma de pagamento: \n");
@@ -11,32 +97,34 @@ int pagar(float valor){
         printf("2 - Cartão\n");
         printf("3 - Dinheiro (desconto de 2%%)\n");
         printf("4 - Crediário\n");
-        printf("####%.2f####", valor);
-        
         scanf("%d", &forma_pagamento);
 
         switch(forma_pagamento)
         {
             case 1:
-                printf("Pagamento efetuado. Desconto de %.2f. Total: %.2f \n", (valor * 0.05), valor - (valor * 0.05));
+                printf("Pagamento efetuado. Desconto de R$%.2f. Total: R$%.2f \n", (valor * 0.05), valor - (valor * 0.05));
                 valor = valor - (valor * 0.05);
-                return main();
+                break;
+
             case 2:
                 printf("Pagamento efetuado.\n");
-                return main();
+                break;
+
             case 3:
-                printf("Pagamento efetuado. Desconto de %.2f. Total: %.2f \n", (valor * 0.02), valor - (valor * 0.02));
-                return main();                
+                printf("Pagamento efetuado. Desconto de R$%.2f. Total: R$%.2f \n", (valor * 0.02), valor - (valor * 0.02));
+                break;   
+
             case 4:
                 printf("Digite seu código de cliente:\n");
                 scanf("%d", &cod_cliente);
-                perfil = vet_crediario[cod_cliente];
-                perfil + valor;
+                vet_crediario[cod_cliente] += valor;
                 printf("R$%.2f adicionados ao seu saldo.\n", valor);
-                printf("## saldo do cliente %d == %.2f\n");
+                break;
+
             default:
                 printf("Entrada Inválida\n");
+                break;
         }
+        return 0;
     }
-    return valor;
 }
